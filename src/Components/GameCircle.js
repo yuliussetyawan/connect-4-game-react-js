d0bbcec5196d2bf4cce92bfd0444771580f699f0
yuliussetyawan <yuliusetyawan@gmail.com>
2023-01-12 22:35:56 +0700

    Square to circle component

--- src/Components/GameCircle.js
+++ src/Components/GameCircle.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 const onClick = (ev, id) => {
-  alert("On Click ", id);
+  alert("On Click " + id);
 };
 
 const GameCircle = ({ id, color, children }) => {
-  console.log(id);
-  return <div style={{backgroundColor: color}} onClick={(ev) => onClick(ev, id)}>{children}</div>;
+  const style = {
+    backgroundColor: color,
+    width: 100,
+    height: 100,
+    margin: 10,
+    borderRadius: "50%",
+  };
+  return (
+    <div style={style} onClick={(ev) => onClick(ev, id)}>
+      {children}
+    </div>
+  );
 };
 
 export default GameCircle;
