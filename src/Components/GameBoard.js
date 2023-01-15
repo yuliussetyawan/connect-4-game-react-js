27267671347447754a3fdb5aedda4528ba7034e7
yuliussetyawan <yuliusetyawan@gmail.com>
2023-01-15 22:24:19 +0700

    Initializing game board

--- src/Components/GameBoard.js
+++ src/Components/GameBoard.js
@@ -1,24 +1,36 @@
 import React, { useState } from "react";
 import GameCircle from "./GameCircle";
 import "../Game.css";
-import { render } from "@testing-library/react";
 
+
+const NO_CIRCLES = 16;
 const NO_PLAYER = 0;
 const PLAYER_1 = 1;
 const PLAYER_2 = 2;
 
 const GameBoard = () => {
   // create 16 array with default value of zero
-  const [gameBoard, setGameBoard] = useState(Array(16).fill(0));
+  const [gameBoard, setGameBoard] = useState(Array(16).fill(NO_PLAYER));
   const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1);
-  console.log(gameBoard)
+  console.log(gameBoard);
+
+  const initBoard = () => {
+    const circles = [];
+    for (let i = 0; i < NO_CIRCLES; i++){
+      circles.push(renderCircle(i));
+    }
+    return circles;
+  }
 
   const circleClicked = (id) => {
     console.log("circle clicked" + id);
 
-    const board = [...gameBoard];
-    board[id] = currentPlayer;
-    setGameBoard(board);
+    setGameBoard(prev => {
+      return prev.map((circle, pos) => {
+        if(pos === id) return currentPlayer;
+        return circle;
+      })
+    })
 
     setCurrentPlayer(currentPlayer === PLAYER_1 ? PLAYER_2 : PLAYER_1);
 
@@ -32,15 +44,7 @@ const GameBoard = () => {
   }
   return (
     <div className="gameBoard">
-     {renderCircle(0)}
-     {renderCircle(1)}
-     {renderCircle(2)}
-     {renderCircle(3)}
-     {renderCircle(4)}
-     {renderCircle(5)}
-     {renderCircle(6)}
-     {renderCircle(7)}
-     {renderCircle(8)}
+     {initBoard()};
     </div>
   );
 };
